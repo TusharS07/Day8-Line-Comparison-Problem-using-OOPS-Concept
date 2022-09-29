0fe1d65fa5630dc89a2a5e5dd7b7317bb61de09e
Salunkhe@07 <salunkhet07@gmail.com>
2022-09-29 17:31:35 +0530

    Display msg

---
+++ LineComparison.java
@@ -0,0 +1,27 @@
+package com.Bridgelabz.LineComparisonOOP;
+import java.util.Scanner;
+
+public class LineComparison {
+    //declare static variable
+    public static int x1;
+    public static int x2;
+    public static int y1;
+    public static int y2;
+
+    public static void main(String[] args) {
+
+        DisplayMsg.displayMsg();                          //call display method and display welcome msg
+
+        System.out.println(); //for empty space
+
+    }
+
+    //display msg
+    static class DisplayMsg {
+        public static void displayMsg() {
+            System.out.println("Welcome to Line Comparison Computation Program ");
+        }
+
+    }
+}
+
